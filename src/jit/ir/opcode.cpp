523d1e6656d11607126f24cd5afe415b7f49fe6c
Ronald Caesar <github43132@proton.me>
2025-11-09 12:47:45 -0400

    jit/ir: Add opcode interface
    Signed-off-by: Ronald Caesar <github43132@proton.me>

--- src/jit/ir/opcode.cpp
+++ src/jit/ir/opcode.cpp
@@ -1,22 +1,12 @@
 #include "opcode.h"
-#include "type.h"
 #include <stddef.h>
 
 #define LOG_MODULE "jit"
 #include "common/logging.h"
 
 namespace pound::jit::ir {
-#define OPCODE_ARGS_TYPES_SIZE 4
-#define OPCODE_ARRAY_SIZE      386
 
-typedef struct
-{
-    const char *name;
-    type_t      type;
-    type_t      arg_types[OPCODE_ARGS_TYPES_SIZE];
-} decoded_opcode_t;
-
-decoded_opcode_t opcodes[OPCODE_ARRAY_SIZE] = {
+decoded_opcode_t g_opcodes[NUM_OPCODE] = {
 #define OPCODE(name, type, ...) \
     decoded_opcode_t { #name, type, { __VA_ARGS__ } },
 #define A32OPC(name, type, ...) \
@@ -31,9 +21,9 @@ decoded_opcode_t opcodes[OPCODE_ARRAY_SIZE] = {
 void
 opcode_init (void)
 {
-    for (size_t i = 0; i < OPCODE_ARRAY_SIZE; ++i)
+    for (size_t i = 0; i < NUM_OPCODE; ++i)
     {
-        LOG_TRACE("Opcode Registered: %s", opcodes[i].name);
+        LOG_TRACE("Opcode Registered: %s", g_opcodes[i].name);
     }
 }
 } // namespace pound::jit::ir
