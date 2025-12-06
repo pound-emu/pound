0d957968dfd50d785af504b5ec3f9520216fce4e
Ronald Caesar <github43132@proton.me>
2025-12-06 01:47:38 -0400

    jit/interpreter: Organize ifdefs
    Signed-off-by: Ronald Caesar <github43132@proton.me>

--- src/jit/interpreter/arm32/instruction.c
+++ src/jit/interpreter/arm32/instruction.c
@@ -1,47 +1,101 @@
 /*
- * THIS FILE IS A WORK IN PROGRESS AND WILL BE RE-WRITTEN
- * Defines pvm_jit_interpreter_instruction_t struct and its internal opcodes.
+ * Defines pvm_jit_interpreter_arm32_instruction_t struct and its internal
+ * opcodes.
  */
 
 #include "frontend/decoder/arm32_opcodes.h"
+#include "common/passert.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+/*
+ * Computed gotos are a GCC/Clang extension that significantly improves
+ * interpreter performance by predicting branch targets.
+ */
 #if defined(__GNUC__) || defined(__clang__)
-    #define HANDLER(name)   HANDLER_##name
-    #define DISPATCH()      do { \
-                                instr++; \
-                                goto *dispatch_table[instr->opcode]; \
-                            } while(0)
+#define PVM_USE_COMPUTED_GOTO 1
 #else
-    #define HANDLER(name)   case name
-    #define DISPATCH()      goto dispatch_loop
+#define PVM_USE_COMPUTED_GOTO 0
 #endif
 
 typedef struct
 {
+    uint32_t GPRs[16];
+    uint32_t PSTATE;
+} pvm_jit_interpreter_arm32_cpu_state_t;
+
+typedef struct
+{
     pvm_jit_decoder_arm32_opcode_t opcode;
-} instruction_t;
+} pvm_jit_interpreter_arm32_instruction_t;
 
 void
-temp(void)
+temp (void)
 {
-    instruction_t *instr = malloc(sizeof(*instr));
-    instr->opcode = PVM_A32_OP_STOP;
-#if defined(__GNUC__) || defined(__clang__)
-    static const void* const dispatch_table[] = {
-        #include "handler_table.inc"
+    pvm_jit_interpreter_arm32_instruction_t *instr = calloc(3, sizeof(*instr));
+    instr->opcode                                  = PVM_A32_OP_ADD_REG;
+    (++instr)->opcode                              = PVM_A32_OP_STOP;
+/*
+ * Uses a jump table with address labels (&&LABEL) to dispatch directly to the
+ * handler.
+ */
+#if PVM_USE_COMPUTED_GOTO
+
+    /* The dispatch table contains the address of every label in handlers.inc */
+    static const void *const dispatch_table[] = {
+#include "handler_table.inc"
     };
 
-    // Initial dispatch
+/*
+ * HANDLER macro defines the label target.
+ * DISPATCH macro increments IP and jumps to the next handler.
+ */
+#define HANDLER(name) name
+#define DISPATCH() \
+    do             \
+    {              \
+        instr++;   \
+    goto *dispatch_table[instr->opcode]; \
+} while (0)
+
+
+    /* Must perform the initial jump to start the race. */
     goto *dispatch_table[instr->opcode];
+
+
+/* Include the instruction logic */
+#include "handlers.inc"
+
+#undef HANDLER
+#undef DISPATCH
+
+/*
+ * Uses a standard switch statement. Slower due to bounds checking and lack of
+ * branch prediction, but 100% portable and safe.
+ */
 #else
-    dispatch_loop:
-    switch (instr->opcode) {
-#endif
 
-    #include "handlers.inc"
+/*
+ * HANDLER macro defines a switch case.
+ * DISPATCH macro jumps back to the switch statement.
+ */
+#define HANDLER(name) case name
+#define DISPATCH()    goto dispatch_loop
 
-#if !defined(__GNUC__) && !defined(__clang__)
-    default: goto HANDLER_PVM_A32_OP_STOP;
+dispatch_loop:
+    switch (instr->opcode)
+    {
+/* Include the instruction logic */
+#include "handlers.inc"
+
+        default:
+            PVM_ASSERT_MSG(
+                0, "Invalid Opcode in interpreter dispatch: %d", instr->opcode);
+            break;
     }
+
+#undef HANDLER
+#undef DISPATCH
 #endif
 }
