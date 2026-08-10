d9ab8f50e096c310e3c8b35fdf6078380dc70090
Ronald Caesar <github43132@proton.me>
2026-08-10 00:19:53 -0400

    [94] core: add error enum

---
+++ src/core/errors.h
@@ -0,0 +1,12 @@
+#ifndef POUND_ERRORS_H
+#define POUND_ERRORS_H
+
+typedef enum
+{
+    POUND_SUCCESS,
+    POUND_ERROR_INVALID_ARGUMENT = -1,
+} error_t;
+
+#endif // POUND_ERRORS_H
+
+/*** end of file ***/
