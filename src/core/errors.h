98ff8d7912719a171880bebbe89ce542931fd12a
Ronald Caesar <github43132@proton.me>
2026-08-10 01:09:06 -0400

    [94] core/errors: add memory alignment error

--- src/core/errors.h
+++ src/core/errors.h
@@ -3,8 +3,15 @@
 
 typedef enum
 {
+    // General Errors
+
     POUND_SUCCESS,
     POUND_ERROR_INVALID_ARGUMENT = -1,
+
+    // Memory Errors
+
+    /// Buffer is not aligned to the required memory alignment.
+    POUND_ERROR_MEMORY_ALIGNMENT = -50,
 } error_t;
 
 #endif // POUND_ERRORS_H
