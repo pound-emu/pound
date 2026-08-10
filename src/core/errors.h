ba4b330416ad4482773c42871cfd3ed4ac79e19e
Ronald Caesar <github43132@proton.me>
2026-08-10 01:36:10 -0400

    [94] core/error: remove explicit numeric values from enums
    This makes it easier to rearrange errors at any time.

--- src/core/errors.h
+++ src/core/errors.h
@@ -6,21 +6,21 @@ typedef enum
     // General Errors
 
     POUND_SUCCESS,
-    POUND_ERROR_INVALID_ARGUMENT = -1,
+    POUND_ERROR_INVALID_ARGUMENT,
 
     // Memory Errors
 
     /// Buffer is not aligned to the required memory alignment.
-    POUND_ERROR_MEMORY_ALIGNMENT = -50,
+    POUND_ERROR_MEMORY_ALIGNMENT,
 
     /// Tried to access memory it was not allowed to access.
-    POUND_ERROR_MEMORY_FAULT = -51,
+    POUND_ERROR_MEMORY_FAULT,
 
     /// Guest Address Size is way to big and caused an overflow.
-    POUND_ERROR_GUEST_ADDRESS_OVERFLOW = -52,
+    POUND_ERROR_GUEST_ADDRESS_OVERFLOW,
 
     /// Guest Address fell outside the mapped memory region.
-    POUND_ERROR_GUEST_ADDRESS_OUT_OF_BOUNDS = -53,
+    POUND_ERROR_GUEST_ADDRESS_OUT_OF_BOUNDS,
 } error_t;
 
 #endif // POUND_ERRORS_H
