ebd2fa196c38739fc6cfb912d200a48a5881a754
Ronald Caesar <github43132@proton.me>
2026-08-10 01:04:33 -0400

    [94] core/attributes: update outdated attributes.h
    The current implementation supports both clang and MSVC. Development
    has now fully switched into supporting clang only.

--- src/core/attributes.h
+++ src/core/attributes.h
@@ -1,3 +1,5 @@
+//! Compiler specific attribute macros.
+
 #ifndef POUND_ATTRIBUTES_H
 #define POUND_ATTRIBUTES_H
 
@@ -6,70 +8,49 @@
 /// aggressively. Cold marks the function as rarely executed.
 ///
 /// Usage:
-/// POUND_HOT bal_error_t emit_instruction(...);
-#if POUND_COMPILER_GCC
-
+/// POUND_HOT POUND_error_t emit_instruction(...);
 #define POUND_HOT  __attribute__((hot))
 #define POUND_COLD __attribute__((cold))
-
-#else
-
-#define POUND_HOT
-#define POUND_COLD
-
-#endif
 
 /// POUND_LIKELY(x)/POUND_UNLIKELY(x)
 /// Hints to the CPU branch predictor. Should only be used in hot functions.
 ///
 /// Usage: if (POUND_UNLIKELY(ptr == NULL)) { ... }
-#if POUND_COMPILER_GCC
-
 #define POUND_LIKELY(x)   __builtin_expect(!!(x), 1)
 #define POUND_UNLIKELY(x) __builtin_expect(!!(x), 0)
-
-#else
-
-#define POUND_LIKELY(x)   (x)
-#define POUND_UNLIKELY(x) (x)
-
-#endif
 
 /// POUND_ALIGNED(x)
 /// Aligns a variable or a structure to x bytes.
 ///
 /// Usage: POUND_ALIGNED(64)  struct data { ... };
-#if POUND_COMPILER_GCC
-
 #define POUND_ALIGNED(x) __attribute__((aligned(x)))
-
-#elif POUND_COMPILER_MSVC
-
-#define POUND_ALIGNED(x) __declspec(align(x))
-
-#else
-
-#define POUND_ALIGNED(x)
-
-#endif
 
 /// POUND_RESTRICT
 /// Tells the compiler that a pointer does not alias any other pointer in
 /// current scope.
-#if POUND_COMPILER_GCC
-
 #define POUND_RESTRICT __restrict__
 
-#elif POUND_COMPILER_MSVC
+/// POUND_EXPORT
+/// Marks a function or global variable for export.
+///
+/// Usage: POUND_EXPORT void POUND_public_api_function(void);
+#define POUND_EXPORT __attribute__((visibility("default")))
 
-#define POUND_RESTRICT __restrict
+/// POUND_WEAK
+/// Marks a function or variable as weak, allowing the host application to override it at link-time
+/// by providing a strong symbol with the same name.
+///
+/// Usage: POUND_WEAK void POUND_default_logger(...);
+#define POUND_WEAK __attribute__((weak))
 
-#else
+/// POUND_THREAD_LOCAL
+/// Declares a variable with thread-local storage duration.
+#define POUND_THREAD_LOCAL _Thread_local
 
-#define POUND_RESTRICT
-
-#endif
-
-#endif // POUND_ATTRIBUTES_H
+/// POUND_NOINLINE
+/// Prevent function inlining.
+///
+/// Usage: POUND_NOINLINE void my_function();
+#define POUND_NOINLINE __attribute__((noinline))
 
 /*** end of file ***/
